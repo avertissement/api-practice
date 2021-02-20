af2bbf9090f492753bb40ed839217d989b3087a7
Elliott Day <eday39@gmail.com>
2021-02-20 00:35:48 -0700

    initial commit

--- script.js
+++ script.js
@@ -5,13 +5,20 @@ function prepareAndAct() {
         e.preventDefault();
         const numberPics = $('.input').find('input').val();
         $('.input').find('input').val('')
+        $('.for-img').empty()
         if(numberPics <= 50 && numberPics >= 1) {
             for(let i=0; i<numberPics; i++) {
                 fetch('https://dog.ceo/api/breeds/image/random')
                 .then(response => response.json())
                 .then(responseJson => addImg(responseJson.message))
-                .catch(error => alert('Sorry, this functionality is not working currently. Please try again later.'));
+                .catch(error => {
+                    alert('Sorry, this functionality is not working currently. Please try again later.')
+                    console.log(error)
+                });
             }
+        }
+        else {
+            $('.for-img').append(`<h4>Please enter a correct number within the interval.</h4>`)
         }
     })
 
